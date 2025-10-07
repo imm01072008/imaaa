cc3e756ce1b551b2873d27935a377391c873bcc1
imm01072008 <siti.fatimah151947@smk.belajar.id>
2025-10-07 12:37:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-conts tombolSapa=document.getElementById('SapaButton)
+conts tombolSapa = document.getElementById('SapaButton);
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah berkunjung!');
+});
